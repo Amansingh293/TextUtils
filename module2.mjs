e356cfdc5c5dc70d712111ceefebbe514ec10338
Amansingh293 <amansinghdl9c@gmail.com>
2022-08-04 21:06:41 +0530

    editing

---
+++ module2.mjs
@@ -0,0 +1,5 @@
+const a = "Aman singh";
+const b = "simran";
+const c = "tanu";
+
+export {a, b , c};
--- src/App.js
+++ src/App.js
@@ -1,31 +1,91 @@
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
+import Navbar from './components/Navbar';
+import About from './components/About';
+import TextForm from './components/Textform';
+import { useState } from 'react';
+import Alert from './components/Alert';
+import ReactDOM from "react-dom/client";
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+} from "react-router-dom";
+
 
 function App() {
+
+  const [mode, changeMode] = useState("light")
+  const [alert, setAlert] = useState(null);
+  const [btnColor, setBtnColor] = useState("info");
+  const [setStyle, setMyStyle] = useState({
+    color: "black",
+    backgroundColor: "white"
+  })
+
+
+  const showAlert = (message, type)=>{
+    setAlert({
+      message: message,
+      type: type
+  })
+  setTimeout(()=>{
+    setAlert(null);},
+    1500
+  )
+
+  }
+  
+  const toggleMode = ()=>{
+    if(mode === "light" ){
+      changeMode("dark");
+      document.body.style.backgroundColor=("rgb(64,64,64)")
+      showAlert("Dark mode has been enabled", "success")
+      setBtnColor("info")
+    }else{
+      changeMode("light")
+      document.body.style.backgroundColor=("white")
+      showAlert("Dark mode has been Disabled", "success");
+      setBtnColor("dark")
+    }
+  }
+
+  const toggleStyle = ()=>{
+    if(setStyle.color === "black"){
+      setMyStyle({
+        color: "white",
+        backgroundColor: "rgb(47, 48, 53)",
+        border: "0.07rem solid white"
+      })
+    }else{
+      setMyStyle({
+        color: "black",
+        backgroundColor: "white"
+      })
+    }
+  }
+
   return (
-    <><nav className="navbar navbar-expand-lg bg-light">
-    <div className="container-fluid">
-      <a className="navbar-brand" href="/">TextUtils</a>
-      <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="/navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-        <span className="navbar-toggler-icon"></span>
-      </button>
-      <div className="collapse navbar-collapse" id="navbarSupportedContent">
-        <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-          <li className="nav-item">
-            <a className="nav-link active" aria-current="page" href="/">Home</a>
-          </li>
-          <li className="nav-item">
-            <a className="nav-link" href="/">About</a>
-          </li>
-        </ul>
-        <form className="d-flex" role="search">
-          <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
-          <button className="btn btn-outline-success" type="submit">Search</button>
-        </form>
-      </div>
-    </div>
-  </nav></>
+    <>
+    <BrowserRouter>
+  <Navbar about="About Us" mode={mode} toggleMode={toggleMode} btnColor={btnColor} toggleStyle={toggleStyle} /> 
+  <Alert alert={alert}/>
+ <div className="container my-3">
+  
+    <Routes>
+      <Route path="/" element={
+      <TextForm heading= "Try TextUtils - Convert lowercase to uppercase, Convert uppercase to lowercase, Remove extra spaces from text and much more" mode={mode} toggleMode={toggleMode}  alert={alert} showAlert={showAlert} btnColor={btnColor} setBtnColor={setBtnColor}/>
+      }/>
+      <Route path="/About" element={
+    <About setStyle={setStyle} mode={mode}/>
+      } />
+    </Routes>
+
+ </div>
+ </BrowserRouter>
+  </>
   );
 }
 
 export default App;
+{/* // greenMode={greenMode} btncolour={btncolour} redMode={redMode} blueMode={blueMode} darkMode={darkMode} */}
